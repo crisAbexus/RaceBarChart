9189cf3cf1b35b8caeaaebb84374cc3aa7360854
Cristian F. Tovar <ctovar@abexuspr.com>
2020-08-26 13:23:35 -0500

    data array

--- src/App.js
+++ src/App.js
@@ -1,26 +1,223 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import * as d3 from 'd3';
+import * as d3array from 'd3-array';
+import { useD3 } from 'd3blackbox';
 
-function App() {
+
+async function drawRace(svg, data) {
+  const width = 954;
+  const barSize = 48;
+  const n = 12;
+  const k = 10;
+  const duration = 250;
+  const margin = ({ top: 16, right: 6, bottom: 6, left: 0 });
+  const height = margin.top + barSize * n + margin.bottom
+  const x = d3.scaleLinear([0, 1], [margin.left, width - margin.right])
+  const y = d3.scaleBand()
+    .domain(d3.range(n + 1))
+    .rangeRound([margin.top, margin.top + barSize * (n + 1 + 0.1)])
+    .padding(0.1)
+
+  const names = new Set(data.map(d => d.name));
+  const prev = new Map(nameframes.flatMap(([, data]) => d3.pairs(data, (a, b) => [b, a])));
+  const nameframes = d3array.groups(keyframes.flatMap(([, data]) => data), d => d.name);
+  const next = new Map(nameframes.flatMap(([, data]) => d3.pairs(data)));
+
+  function axis(svg) {
+    const g = svg.append("g")
+        .attr("transform", `translate(0,${margin.top})`);
+  
+    const axis = d3.axisTop(x)
+        .ticks(width / 160)
+        .tickSizeOuter(0)
+        .tickSizeInner(-barSize * (n + y.padding()));
+  
+    return (_, transition) => {
+      g.transition(transition).call(axis);
+      g.select(".tick:first-of-type text").remove();
+      g.selectAll(".tick:not(:first-of-type) line").attr("stroke", "white");
+      g.select(".domain").remove();
+    };
+  }
+
+  function textTween(a, b) {
+    const i = d3.interpolateNumber(a, b);
+    return function(t) {
+      this.textContent = formatNumber(i(t));
+    };
+  }
+
+  const formatNumber = d3.format(",d")
+
+  function labels(svg) {
+    let label = svg.append("g")
+        .style("font", "bold 12px var(--sans-serif)")
+        .style("font-variant-numeric", "tabular-nums")
+        .attr("text-anchor", "end")
+      .selectAll("text");
+  
+    return ([date, data], transition) => label = label
+      .data(data.slice(0, n), d => d.name)
+      .join(
+        enter => enter.append("text")
+          .attr("transform", d => `translate(${x((prev.get(d) || d).value)},${y((prev.get(d) || d).rank)})`)
+          .attr("y", y.bandwidth() / 2)
+          .attr("x", -6)
+          .attr("dy", "-0.25em")
+          .text(d => d.name)
+          .call(text => text.append("tspan")
+            .attr("fill-opacity", 0.7)
+            .attr("font-weight", "normal")
+            .attr("x", -6)
+            .attr("dy", "1.15em")),
+        update => update,
+        exit => exit.transition(transition).remove()
+          .attr("transform", d => `translate(${x((next.get(d) || d).value)},${y((next.get(d) || d).rank)})`)
+          .call(g => g.select("tspan").tween("text", d => textTween(d.value, (next.get(d) || d).value)))
+      )
+      .call(bar => bar.transition(transition)
+        .attr("transform", d => `translate(${x(d.value)},${y(d.rank)})`)
+        .call(g => g.select("tspan").tween("text", d => textTween((prev.get(d) || d).value, d.value))));
+  }
+
+  const color = () => {
+    const scale = d3.scaleOrdinal(d3.schemeTableau10);
+    if (data.some(d => d.category !== undefined)) {
+      const categoryByName = new Map(data.map(d => [d.name, d.category]))
+      scale.domain(Array.from(categoryByName.values()));
+      return d => scale(categoryByName.get(d.name));
+    }
+    return d => scale(d.name);
+  }
+
+  function rank(value) {
+    const data = Array.from(names, name => ({name, value: value(name)}));
+    data.sort((a, b) => d3.descending(a.value, b.value));
+    for (let i = 0; i < data.length; ++i) data[i].rank = Math.min(n, i);
+    return data;
+  }
+
+  const keyframes = () => {
+    const keyframes = [];
+    let ka, a, kb, b;
+    for ([[ka, a], [kb, b]] of d3.pairs(datevalues)) {
+      for (let i = 0; i < k; ++i) {
+        const t = i / k;
+        keyframes.push([
+          new Date(ka * (1 - t) + kb * t),
+          rank(name => (a.get(name) || 0) * (1 - t) + (b.get(name) || 0) * t)
+        ]);
+      }
+    }
+    keyframes.push([new Date(kb), rank(name => b.get(name) || 0)]);
+    return keyframes;
+  }
+
+  const datevalues = Array.from(d3array.rollup(data, ([d]) => d.value, d => +d.date, d => d.name))
+    .map(([date, data]) => [new Date(date), data])
+    .sort(([a], [b]) => d3.ascending(a, b))
+
+  function ticker(svg) {
+    const now = svg.append("text")
+      .style("font", `bold ${barSize}px var(--sans-serif)`)
+      .style("font-variant-numeric", "tabular-nums")
+      .attr("text-anchor", "end")
+      .attr("x", width - 6)
+      .attr("y", margin.top + barSize * (n - 0.45))
+      .attr("dy", "0.32em")
+      .text(formatDate(keyframes[0][0]));
+
+    return ([date], transition) => {
+      transition.end().then(() => now.text(formatDate(date)));
+    };
+  }
+
+  const formatDate = d3.utcFormat("%Y")
+
+  function bars(svg) {
+    let bar = svg.append("g")
+      .attr("fill-opacity", 0.6)
+      .selectAll("rect");
+
+    return ([date, data], transition) => bar = bar
+      .data(data.slice(0, n), d => d.name)
+      .join(
+        enter => enter.append("rect")
+          .attr("fill", color)
+          .attr("height", y.bandwidth())
+          .attr("x", x(0))
+          .attr("y", d => y((prev.get(d) || d).rank))
+          .attr("width", d => x((prev.get(d) || d).value) - x(0)),
+        update => update,
+        exit => exit.transition(transition).remove()
+          .attr("y", d => y((next.get(d) || d).rank))
+          .attr("width", d => x((next.get(d) || d).value) - x(0))
+      )
+      .call(bar => bar.transition(transition)
+        .attr("y", d => y(d.rank))
+        .attr("width", d => x(d.value) - x(0)));
+  }
+
+  svg = svg.attr("viewBox", [0, 0, width, height]);
+
+  const updateBars = bars(svg);
+  const updateAxis = axis(svg);
+  const updateLabels = labels(svg);
+  const updateTicker = ticker(svg);
+
+  svg.node();
+
+  for (const keyframe of keyframes) {
+    const transition = svg.transition()
+      .duration(duration)
+      .ease(d3.easeLinear);
+
+    // Extract the top bar’s value.
+    x.domain([0, keyframe[1][0].value]);
+
+    updateAxis(keyframe, transition);
+    updateBars(keyframe, transition);
+    updateLabels(keyframe, transition);
+    updateTicker(keyframe, transition);
+
+
+  }
+}
+
+export default function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    async function loadData() {
+      const csv = await d3.csv('category-brands.csv')
+      let data = csv
+      csv.forEach((d, idx)=>{
+        const date = new Date(d.date)
+        data[idx].date = date.toLocaleString('en-CA').split(',')[0]
+      })
+      console.log('App.js - data:');
+      console.log(data);
+      debugger
+      setData(data)
+    }
+    loadData();
+
+  });
+
+  const svgRef = useD3(anchor => {
+    if (data !== null) {
+      console.log('data:');
+      console.log(data)
+      
+      drawRace(d3.select(anchor), data)
+    }
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <svg width='1024' height='1024' ref={svgRef} />
     </div>
   );
 }
 
-export default App;
