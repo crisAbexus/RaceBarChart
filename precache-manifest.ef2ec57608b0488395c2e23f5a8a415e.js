f2104cf6449b5dcdb01acfc4ea3d1a4c7e024311
Cristian F. Tovar <ctovar@abexuspr.com>
2020-08-27 15:42:16 -0500

    Updates

---
+++ precache-manifest.ef2ec57608b0488395c2e23f5a8a415e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0004a3eef6c8aa80fe6f99d9f485dd65",
+    "url": "/RaceBarChart/index.html"
+  },
+  {
+    "revision": "2c827843b2560fb6cf64",
+    "url": "/RaceBarChart/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "876a2d9750c403363878",
+    "url": "/RaceBarChart/static/js/2.97a0c57c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RaceBarChart/static/js/2.97a0c57c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c827843b2560fb6cf64",
+    "url": "/RaceBarChart/static/js/main.c7559f71.chunk.js"
+  },
+  {
+    "revision": "a3f706d79bafef9178af",
+    "url": "/RaceBarChart/static/js/runtime-main.ea667c48.js"
+  }
+]);
